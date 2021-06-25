a102118a8776aa2fb2e1b511a75be616f10cd4be
Natiq <xelilinatiq@gmail.com>
2021-06-26 01:06:26 +0400

    commit -m "language buttons added"

--- freeapp/src/containers/Portfolio.js
+++ freeapp/src/containers/Portfolio.js
@@ -3,6 +3,9 @@ import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import * as commonActions from "../redux/actions/Common";
 import * as userActions from "../redux/actions/User";
+import * as Icon from "react-icons/fi";
+
+//Images
 import dashPhoto from "../assets/img/dashPhoto.jpg";
 import backPhoto from "../assets/img/bg-techno.png";
 import adnsu from "../assets/img/logos/adnsu-logo-new.png";
@@ -11,7 +14,8 @@ import school15 from "../assets/img/logos/school_15.jfif";
 import school44 from "../assets/img/logos/school_44.jfif";
 import itmim from "../assets/img/logos/itmim.png";
 import dsx from "../assets/img/logos/dsx.png";
-import * as Icon from "react-icons/fi"
+import flag_eng from "../assets/icons/british-flag.jpg";
+import flag_aze from "../assets/icons/azerbaijan-flag.ico";
 
 class Portfolio extends Component {
     constructor() {
@@ -58,9 +62,13 @@ class Portfolio extends Component {
                 className="tw-w-screen tw-h-auto tw-bg-cover tw-bg-no-repeat bg-img-overlay"
                 style={{ backgroundImage: `url(${backPhoto})` }}
             >
+                <div className="tw-justify-end tw-pt-4 tw-pb-2 tw-pr-10 tw-fixed tw-z-20 tw-right-0">
+                    <div className="brdr-b tw-px-2 tw-bg-gray-400">AZE<img src={flag_aze} className="tw-ml-1" width={20} alt="aze" /></div>
+                    <div className="brdr-b tw-px-2 tw-bg-gray-400">ENG<img src={flag_eng} className="tw-ml-1" width={20} alt="eng" /></div>
+                </div>
                 <div className="flx-row">
                     <div className="tw-w-full tw-flex tw-flex-col tw-overflow-y-auto tw-h-full">
-                        <div className="flx-row tw-bg-white tw-m-32 tw-rounded tw-border tw-border-gray-600 p-3 tw-z-10">
+                        <div className="flx-row tw-bg-white tw-mx-32 tw-mb-20 tw-mt-10 tw-rounded tw-border tw-border-gray-600 p-3 tw-z-10">
                             <div className="tw-flex-1 tw-text-left tw-align-middle tw-m-auto tw-pl-40">
                                 <div className="tw-text-6xl tw-font-serif tw-text-gray-900 tw-font-bold">Natig Khalili</div>
                                 <div className="tw-text-2xl tw-text-gray-700">Middle Full-Stack Web Developer (Node, React, Asp.net)</div>
@@ -87,23 +95,23 @@ class Portfolio extends Component {
                                     <div className="col-sm-6 tw-pb-10 tw-h-fit-content  tw-px-4">
                                         <label className="tw-text-2xl">Frequently used</label>
                                         <div className="row tw-justify-center">
-                                            <div className="animated fadeInUp brdr-b tw-px-2 tw-bg-red-700">HTML</div>
-                                            <div className="animated fadeInDown brdr-b tw-px-2 tw-bg-red-400">CSS</div>
-                                            <div className="animated fadeInLeft brdr-b tw-px-2 tw-bg-orange-700">ReactJS</div>
-                                            <div className="animated fadeInLeft brdr-b tw-px-2 tw-bg-orange-900">NodeJS</div>
-                                            <div className="animated fadeInLeft brdr-b tw-px-2 tw-bg-orange-900">ArcGisJS</div>
-                                            <div className="animated fadeInLeft brdr-b tw-px-2 tw-bg-blue-700">Asp.Net</div>
-                                            <div className="animated fadeInLeft brdr-b tw-px-2 tw-bg-blue-900">C#</div>
-                                            <div className="animated fadeInDown brdr-b tw-px-2 tw-bg-black">GIT</div>
+                                            <div className="animated fadeInLeftBig brdr-b tw-px-2 tw-bg-red-700">HTML</div>
+                                            <div className="animated fadeInLeft brdr-b tw-px-2 tw-bg-red-400">CSS</div>
+                                            <div className="animated flipInX brdr-b tw-px-2 tw-bg-indigo-600">ReactJS</div>
+                                            <div className="animated flipInY brdr-b tw-px-2 tw-bg-orange-900">NodeJS</div>
+                                            <div className="animated fadeInUp brdr-b tw-px-2 tw-bg-yellow-500">ArcGisJS</div>
+                                            <div className="animated fadeInDownBig brdr-b tw-px-2 tw-bg-blue-700">Asp.Net</div>
+                                            <div className="animated fadeInRight brdr-b tw-px-2 tw-bg-blue-900">C#</div>
+                                            <div className="animated fadeInRightBig brdr-b tw-px-2 tw-bg-black">GIT</div>
                                             <div className="animated fadeInDown brdr-b tw-px-2 tw-bg-red-600">NPM</div>
+                                            <div className="animated fadeInUpBig brdr-b tw-px-2 tw-bg-red-600">Oracle</div>
                                         </div>
                                         <label className="tw-text-2xl tw-pt-2">Sometimes used</label>
                                         <div className="row tw-justify-center">
-                                            <div className="animated fadeInUp brdr-b tw-px-2 tw-bg-red-700">jQuery</div>
-                                            <div className="animated fadeInUp brdr-b tw-px-2 tw-bg-red-700">Ajax</div>
-                                            <div className="animated fadeInDown brdr-b tw-px-2 tw-bg-red-600">Oracle</div>
-                                            <div className="animated fadeInDown brdr-b tw-px-2 tw-bg-red-600">SASS</div>
-                                            <div className="animated fadeInDown brdr-b tw-px-2 tw-bg-red-600">Photoshop</div>
+                                            <div className="animated fadeInLeft brdr-b tw-px-2 tw-bg-orange-600">jQuery</div>
+                                            <div className="animated fadeInUp brdr-b tw-px-2 tw-bg-red-400">SASS</div>
+                                            <div className="animated fadeInDown brdr-b tw-px-2 tw-bg-red-600">MS-SQL</div>
+                                            <div className="animated fadeInRight brdr-b tw-px-2 tw-bg-blue-800">Photoshop</div>
                                         </div>
                                     </div>
                                 </div>
@@ -113,8 +121,8 @@ class Portfolio extends Component {
                             backgroundImage: '-webkit-linear-gradient(0deg,#766dff 0%,#88f3ff 100%)'
                         }}>
                             <div className="flx-row tw-justify-center tw-py-20">
-                                <button type="button" className={`btn btn-lg ${timeline === 'education' ? 'btn-light' : 'tw-bg-blue-300 tw-text-white'} tw-text-lg mx-1 tw-px-20 tw-py-4`} onClick={() => { this.setState({ timeline: 'education' }) }}>Təhsil</button>
-                                <button type="button" className={`btn btn-lg ${timeline === 'work' ? 'btn-light' : 'tw-bg-blue-300 tw-text-white'} tw-text-lg mx-1 tw-px-20 tw-py-4`} onClick={() => { this.setState({ timeline: 'work' }) }}>İş təcrübəsi</button>
+                                <button type="button" className={`btn btn-lg ${timeline === 'education' ? 'btn-light' : 'tw-bg-blue-300 tw-text-white'} tw-text-lg mx-1 tw-px-20 tw-py-4`} onClick={() => { this.setState({ timeline: 'education' }) }}>Education</button>
+                                <button type="button" className={`btn btn-lg ${timeline === 'work' ? 'btn-light' : 'tw-bg-blue-300 tw-text-white'} tw-text-lg mx-1 tw-px-20 tw-py-4`} onClick={() => { this.setState({ timeline: 'work' }) }}>Work experience</button>
                             </div>
                             {timeline === "education" &&
                                 <div className="timeline tw-mb-20">
@@ -123,7 +131,7 @@ class Portfolio extends Component {
                                             <div className="tw-flex">
                                                 <div className="tw-flex-1">
                                                     <h5>2001-2006</h5>
-                                                    <p>Gəncə şəhəri, 15 nömrəli tam orta məktəb</p>
+                                                    <p>Azerbaijan, Ganja, Secondary school number 15</p>
                                                 </div>
                                                 <img src={school15} alt="" className="tw-w-1/5 tw-rounded tw-ml-2" />
                                             </div>
@@ -134,7 +142,7 @@ class Portfolio extends Component {
                                             <div className="tw-flex">
                                                 <div className="tw-flex-1">
                                                     <h5>2006-2009</h5>
-                                                    <p>Gəncə şəhəri, 44 saylı Bağça məktəb lisey kompleksi</p>
+                                                    <p>Azerbaijan, Ganja, Secondary school number 44</p>
                                                 </div>
                                                 <img src={school44} alt="" className="tw-w-1/5 tw-rounded tw-ml-2" />
                                             </div>
@@ -145,7 +153,7 @@ class Portfolio extends Component {
                                             <div className="tw-flex">
                                                 <div className="tw-flex-1">
                                                     <h5>2009-2012</h5>
-                                                    <p>Gəncə şəhəri, 15 nömrəli tam orta məktəb</p>
+                                                    <p>Azerbaijan, Ganja, Secondary school number 15</p>
                                                 </div>
                                                 <img src={school15} alt="" className="tw-w-1/5 tw-rounded tw-ml-2" />
                                             </div>
@@ -156,7 +164,7 @@ class Portfolio extends Component {
                                             <div className="tw-flex">
                                                 <div className="tw-flex-1">
                                                     <h5>2012-2016</h5>
-                                                    <p>Azərbaycan Dövlət İqtisad Universiteti</p>
+                                                    <p>Azerbaijan State Economic University</p>
                                                 </div>
                                                 <img src={unec} alt="" className="tw-w-1/5 tw-rounded tw-ml-2" />
                                             </div>
@@ -167,7 +175,7 @@ class Portfolio extends Component {
                                             <div className="tw-flex">
                                                 <div className="tw-flex-1">
                                                     <h5>2016-2018</h5>
-                                                    <p>Azərbaycan Dövlət Neft və Sənaye Universiteti</p>
+                                                    <p>Azerbaijan State Oil and Industry University</p>
                                                 </div>
                                                 <img src={adnsu} alt="" className="tw-w-1/5 tw-rounded tw-ml-2" />
                                             </div>
@@ -182,7 +190,7 @@ class Portfolio extends Component {
                                             <div className="tw-flex">
                                                 <div className="tw-flex-1">
                                                     <h5>2017-2019</h5>
-                                                    <p>İnformasiya texnologiyaları və məlumat idarəetmə mərkəzi</p>
+                                                    <p>Information technologies and Data Management Center</p>
                                                 </div>
                                                 <img src={itmim} alt="" className="tw-w-1/5 tw-rounded tw-ml-2" />
                                             </div>
@@ -193,7 +201,7 @@ class Portfolio extends Component {
                                             <div className="tw-flex">
                                                 <div className="tw-flex-1">
                                                     <h5>2019-2020</h5>
-                                                    <p>Hərbi xidmət</p>
+                                                    <p>Military Service</p>
                                                 </div>
                                                 <img src={dsx} alt="" className="tw-w-1/5 tw-rounded tw-ml-2" />
                                             </div>
@@ -204,7 +212,7 @@ class Portfolio extends Component {
                                             <div className="tw-flex">
                                                 <div className="tw-flex-1">
                                                     <h5>2020-NOW</h5>
-                                                    <p>İnformasiya texnologiyaları və məlumat idarəetmə mərkəzi</p>
+                                                    <p>Information technologies and Data Management Center</p>
                                                 </div>
                                                 <img src={itmim} alt="" className="tw-w-1/5 tw-rounded tw-ml-2" />
                                             </div>
